caa04300cef0b1a5439968c4702bccbb1cf81e33
İsmail Özçelik <ismailozcelik2006@gmail.com>
2022-04-08 06:25:36 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-site: 'https://iozcelik.github.io/iozcelik/',
+site: 'https://iozcelik.github.io/',
   markdown: {
     drafts: true,
   },
