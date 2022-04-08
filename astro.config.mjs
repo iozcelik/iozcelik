595f92548734270d1436898a2e6a19ec78212a6d
İsmail Özçelik <ismail.ozcelik@hayenteknoloji.com>
2022-04-08 10:40:01 +0300

    basic updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-site: 'https://iozcelik.github.io/',
   markdown: {
-    drafts: true,
+    shikiConfig: {
+      theme: 'nord',
+      wrap: true,
+    },
   },
   integrations: [tailwind(), sitemap()],
 });
